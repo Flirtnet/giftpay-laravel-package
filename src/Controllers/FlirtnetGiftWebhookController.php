daeca81f97ac1203b4eb47569c783c85557fc0db
Flirtnet <32797736+Flirtnet@users.noreply.github.com>
2024-02-12 00:57:36 +0100

    Create FlirtnetGiftWebhookController.php

---
+++ src/Controllers/FlirtnetGiftWebhookController.php
@@ -0,0 +1,14 @@
+// FlirtnetGiftWebhookController.php
+
+namespace YourNamespace\FlirtnetGift\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FlirtnetGiftWebhookController extends Controller
+{
+    public function handleWebhook(Request $request)
+    {
+        // Implement webhook handling logic here
+    }
+}
