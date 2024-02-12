91999bad3372eb9cf74524608f316e16c6be9168
Flirtnet <32797736+Flirtnet@users.noreply.github.com>
2024-02-12 01:11:14 +0100

    Create FlirtnetGiftValidationController.php

---
+++ src/Controllers/FlirtnetGiftValidationController.php
@@ -0,0 +1,15 @@
+<?php
+// FlirtnetGiftValidationController.php
+
+namespace YourNamespace\FlirtnetGift\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FlirtnetGiftValidationController extends Controller
+{
+    public function validatePayment(Request $request)
+    {
+        // Implement payment validation logic here
+    }
+}
