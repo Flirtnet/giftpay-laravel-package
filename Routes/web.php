a362cbe36a5f38c5e78cc59ac070d462218fe424
Flirtnet <32797736+Flirtnet@users.noreply.github.com>
2024-02-12 01:09:17 +0100

    Delete Routes/web.php

--- Routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-// web.php
-
-use Illuminate\Support\Facades\Route;
-use YourNamespace\FlirtnetGift\Controllers\FlirtnetGiftValidationController;
-
-Route::post('/payment/validate', [FlirtnetGiftValidationController::class, 'validatePayment']);
