243a278c20d60f25ebca86dd87fd8e16f9d9c23d
Flirtnet <32797736+Flirtnet@users.noreply.github.com>
2024-02-12 01:09:09 +0100

    Delete Routes/api.php

--- Routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-// api.php
-
-use Illuminate\Support\Facades\Route;
-use YourNamespace\FlirtnetGift\Controllers\FlirtnetGiftWebhookController;
-
-Route::post('/webhook', [FlirtnetGiftWebhookController::class, 'handleWebhook']);
