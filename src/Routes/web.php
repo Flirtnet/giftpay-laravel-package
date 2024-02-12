96c3325cb7e0ff45ab0209e6401be16b6b8afbe4
Flirtnet <32797736+Flirtnet@users.noreply.github.com>
2024-02-12 01:19:46 +0100

    Create web.php

---
+++ src/Routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use YourNamespace\FlirtnetGift\Controllers\FlirtnetGiftValidationController;
+
+Route::post('/payment/validate', [FlirtnetGiftValidationController::class, 'validatePayment']);
